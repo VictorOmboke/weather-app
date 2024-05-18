86699a13f1d7523da140e1dd7c7506e627492930
Victor Omboke <victoromboke@gmail.com>
2024-05-17 19:55:39 -0700

    Fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
             loader: "file-loader",
             options: {
               name: "[search].[svg]",
-              outPath: "images/",
+              outputPath: "images/",
             },
           },
         ],
